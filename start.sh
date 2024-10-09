576b3d311d9361140927eab7ded3d8c60563c041
satoshi morishita <morisita@uniuni.co.jp>
2024-10-09 17:54:40 +0900

    fixed an error in the docker compose command

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose build --no-cache
+docker compose build --no-cache
 docker compose up -d
+sleep 3
 docker compose logs -t
-docker-compose down --rmi all --volumes --remove-orphans
-
+docker compose down --rmi all --volumes --remove-orphans
